bac637a4acc61ebce7278875582fa35a882d49ad
hwrdwlf <hwrdwlf@gmail.com>
2015-03-20 09:36:25 -0700

    Update test.php

--- test.php
+++ test.php
@@ -3,11 +3,15 @@
 require("CurrencyController.php");
 
 $url = 'https://wikitech.wikimedia.org/wiki/Fundraising/tech/Currency_conversion_sample?ctype=text/xml&action=raw';
+// These database attributes would normally go in another file like bootstrap or some config file
 $user = 'wmf_user';
 $pass = 'password';
 $socket = 'unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;dbname=wmf';
 
 try {
+    // I have this hard coded again here because of some mix up of the local MYSQL install and 
+    // the MAMP mysql installation in parallel
+    // For whatever reason this was giving my some issues
     $dbh = new PDO("mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;dbname=wmf", $user, $pass);
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
@@ -17,8 +21,9 @@ $params = array(
 	'dbh' => $dbh
 );
 
-$cc = new CurrencyController($params);
 try{
+	// instantiate our class
+	$cc = new CurrencyController($params);
 	// Get data from the API
 	$data = $cc->getData($url);
 	// Parse data into an array
